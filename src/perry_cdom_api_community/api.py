b4afe6e6de6de8ebc86323c8728e6f63c903011e
ivancoppa <ivan.coppa@gmail.com>
2024-10-23 22:18:16 +0200

    Move http request away from entity into http client (#3)

--- src/perry_cdom_api_community/api.py
+++ src/perry_cdom_api_community/api.py
@@ -1,8 +1,15 @@
+import json
 import logging
+from typing import Optional
 
 from aiohttp import ClientSession
+from aiohttp.client_exceptions import ClientResponseError
 
-from perry_cdom_api_community.const import PERRY_CDOM_BASE_URL, PERRY_CDOM_GET_INFO_URL
+from perry_cdom_api_community.const import (
+    PERRY_CDOM_BASE_URL,
+    PERRY_CDOM_GET_INFO_URL,
+    PERRY_CDOM_SET_WORKING_MODE,
+)
 from perry_cdom_api_community.entity import PerryThermostat
 from perry_cdom_api_community.http_request import PerryHTTPRequest
 
@@ -20,22 +27,61 @@ class PerryCdomCrm4API:
         self.pin = pin
         self.host = PERRY_CDOM_BASE_URL
         self.api = PerryHTTPRequest(self.session, self.cdom_serial_number, self.pin)
-        self.perry_thermostat: PerryThermostat
+        self.perry_thermostat: Optional[PerryThermostat] = None
 
-    async def async_get_thermostat(self) -> PerryThermostat:
-        """Return the appliances."""
-
-        resp = await self.api.request("post", PERRY_CDOM_GET_INFO_URL)
-        resp.raise_for_status()
-        data = await resp.json()
-        if data["communicationStatus"] == -3:
-            raise Exception("Error authenticating: " + data["Message"])
-
-        self.perry_thermostat = PerryThermostat(
-            self.cdom_serial_number, self.api, await resp.json()
-        )
-        return self.perry_thermostat
-
-    async def async_set_thermostat(self, changes) -> PerryThermostat:
+    async def set_thermostat(self, thermo_zone_container, changes) -> bool:
         """Change the appliances."""
-        return await self.perry_thermostat.send_command(changes)
+
+        data = thermo_zone_container | changes
+
+        if "CdomSerialNumber" in data:
+            del data["CdomSerialNumber"]
+        if "CreationDate" in data:
+            del data["CreationDate"]
+        if "easyModeCoolingActivationTime" in data:
+            del data["easyModeCoolingActivationTime"]
+        if "easyModeCoolingSwitchOffTime" in data:
+            del data["easyModeCoolingSwitchOffTime"]
+        if "easyModeHeatingActivationTime" in data:
+            del data["easyModeHeatingActivationTime"]
+        if "easyModeHeatingSwitchOffTime" in data:
+            del data["easyModeHeatingSwitchOffTime"]
+
+        payload = {
+            "ThermoZonesContainer": json.dumps(data)  # The modified zones container
+        }
+
+        resp = await self.api.request("post", PERRY_CDOM_SET_WORKING_MODE, json=payload)
+        try:
+            resp.raise_for_status()
+            data = await resp.json()
+            _LOGGER.info(
+                f"Response from thermostat {self.cdom_serial_number}: {data}\n"
+            )
+            return True
+        except ClientResponseError as e:
+            _LOGGER.error(
+                f"Error sending command '{thermo_zone_container}' to thermostat {self.cdom_serial_number} : {e}"
+            )
+            # raise e
+            return False
+
+        return False
+
+    async def get_thermoreg(self):
+        """Update the thermostat data."""
+        _LOGGER.info("PerryCdomCrm4API: loading data from server")
+        resp = await self.api.request("post", PERRY_CDOM_GET_INFO_URL)
+        try:
+            resp.raise_for_status()
+            data = await resp.json()
+
+            if data["communicationStatus"] == -3:
+                raise Exception("Error authenticating: " + data["Message"])
+            return data
+
+        except ClientResponseError as e:
+            _LOGGER.error(
+                f"Error getting data from thermostat {self._cdom_serial_number} : {e}"
+            )
+            raise e
